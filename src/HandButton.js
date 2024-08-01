57572568298cc1816d4660444e2e90b3fffefa72
KimChungO <kco796@naver.com>
2024-08-01 13:48:14 +0900

    인라인 CSS

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,12 +1,27 @@
 import React from "react";
 import HandIcon from "./HandIcon";
+import purple from "./assets/purple.svg";
+
+const HandButtonStyle = {
+    width: "166px",
+    height: "166px",
+    border: "none",
+    outline: "none",
+    textAlign: "center",
+    cursor: "pointer",
+    backgroundColor: "transparent",
+    backgroundImage: `url('${purple}')`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "contain",
+};
 
 const HandButton = ({ value, onClick }) => {
     const handleClick = () => {
         onClick(value);
     };
     return (
-        <button onClick={handleClick}>
+        <button style={HandButtonStyle} onClick={handleClick}>
             <HandIcon value={value} />
         </button>
     );
