8ed7dbe41b9f15f90fe10dc7291e3aa1ebfb7f26
KimChungO <kco796@naver.com>
2024-08-01 11:52:53 +0900

    초기화 버튼

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Button from "./Button";
 import HandButton from "./HandButton";
 
 const App = () => {
     const handleClick = (value) => console.log(value);
+    const handleClearClick = () => console.log("처음부터");
     return (
         <div className="App">
+            <Button onClick={handleClearClick}>처음부터</Button>
             <HandButton value="rock" onClick={handleClick} />
             <HandButton value="scissor" onClick={handleClick} />
             <HandButton value="paper" onClick={handleClick} />
