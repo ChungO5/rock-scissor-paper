8dfbbe8456426bea1cbec5500b2eae6524a23aab
KimChungO <kco796@naver.com>
2024-08-01 12:07:22 +0900

    패고르기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./Button";
 import HandButton from "./HandButton";
+import { compareHand, generateRandomHand } from "./utils";
+import HandIcon from "./HandIcon";
+
+const getResult = (me, other) => {
+    const comparison = compareHand(me, other);
+    if (comparison > 0) return "승리";
+    if (comparison < 0) return "패배";
+    return "무승부";
+};
 
 const App = () => {
-    const handleClick = (value) => console.log(value);
-    const handleClearClick = () => console.log("처음부터");
+    const [hand, setHand] = useState("rock");
+    const [otherHand, setOtherHand] = useState("scissor");
+
+    const handleClick = (nexthand) => {
+        setHand(nexthand);
+        setOtherHand(generateRandomHand());
+    };
+
+    const handleClearClick = () => {
+        setHand("rock");
+        setOtherHand("rock");
+    };
+
     return (
         <div className="App">
             <Button onClick={handleClearClick}>처음부터</Button>
+            <p>{getResult(hand, otherHand)}</p>
+            <div>
+                <HandIcon value={hand} />
+                <HandIcon value={otherHand} />
+            </div>
             <HandButton value="rock" onClick={handleClick} />
             <HandButton value="scissor" onClick={handleClick} />
             <HandButton value="paper" onClick={handleClick} />
